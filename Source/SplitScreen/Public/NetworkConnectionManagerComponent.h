78cf1f3c60b2ef0c270657392db39c79232de142
Тимур Комолов <komolov.timurka@mail.ru>
2024-11-14 21:10:40 +0300

    Add choice of game type

---
+++ Source/SplitScreen/Public/NetworkConnectionManagerComponent.h
@@ -0,0 +1,31 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/ActorComponent.h"
+#include "NetworkConnectionManagerComponent.generated.h"
+
+
+UCLASS(ClassGroup=(Custom), meta=(BlueprintSpawnableComponent))
+class SPLITSCREEN_API UNetworkConnectionManagerComponent : public UActorComponent
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this component's properties
+	UNetworkConnectionManagerComponent();
+
+protected:
+	// Called when the game starts
+	virtual void BeginPlay() override;
+
+public:
+	// Called every frame
+	virtual void TickComponent(float DeltaTime, ELevelTick TickType,
+							   FActorComponentTickFunction* ThisTickFunction) override;
+
+	/// Returns user's local IP address or empty string if unable.
+	UFUNCTION(BlueprintCallable, Category = "Network")
+	FString GetLocalIP();
+};
